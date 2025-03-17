3dab2c7bd637c9c2914c6f691cab1790691afc67
Aditya Rathi <quic_aditrath@quicinc.com>
2025-03-17 12:24:58 +0000

    posal: linux: Remove data logging stubs
    Remove not needed linux posal data logging stubs.
    
    Signed-off-by: Aditya Rathi <quic_aditrath@quicinc.com>

--- fwk/platform/posal/src/linux/posal_linux_stubs.c
+++ fwk/platform/posal/src/linux/posal_linux_stubs.c
@@ -40,27 +40,6 @@ ar_result_t core_drv_reset(){
    return AR_EOK;
 }
 
-bool_t posal_data_log_code_status(uint32_t log_code){
-   return false;
-}
-
-ar_result_t posal_data_log_alloc_commit(posal_data_log_info_t *log_info_ptr){
-   ar_result_t result = AR_EOK;
-   return result;
-}
-
-void *posal_data_log_alloc(uint32_t buf_Size, uint32_t log_code, posal_data_log_format_t data_fmt){
-   return NULL;
-}
-
-uint32_t posal_data_log_get_max_buf_size(){
-   return 0;
-}
-
-ar_result_t posal_data_log_commit(void *log_pkt_payload_ptr, posal_data_log_info_t *log_info_ptr){
-   return AR_EOK;
-}
-
 POSAL_HEAP_ID posal_private_get_island_heap_id_v2(uint32_t island_heap_type)
 {
    return spf_mem_island_heap_id;
